b6c5cae98af3fc488180b27274509d7cc1e7bab3
BboyPattyCakes <pjjones05@comcast.net>
2020-04-26 22:44:53 -0400

    Source Tree - Set All C++ Header Files to forward declarations.

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -4,11 +4,11 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
-#include "TankAimingComponent.h"
 #include "Tank.generated.h"
 
 
 class UTankBarrel;
+class UTankAimingComponent;
 
 //Hold Barrel's Properties and Elevate Methods
 UCLASS()
